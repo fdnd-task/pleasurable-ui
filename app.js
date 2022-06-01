9ca8bf630c2d456384279aa6e32aaf821f04d7b8
DphnZwp <zwuupdaphne@gmail.com>
2022-06-02 00:50:18 +0200

    send messages with date

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const path = require('path')
-const compression = require('compression')
 const app = express()
+const http = require('http').createServer(app)
+const path = require('path')
+const io = require('socket.io')(http)
+const compression = require('compression')
 const PORT = process.env.PORT || 3000
 
 // Routes
@@ -13,6 +15,18 @@ app.set('views', path.join(__dirname + '/views'))
 app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname + '/public')))
 
+io.on('connection', (socket) => {
+  console.log('a user connected')
+
+  socket.on('chat', (data) => {
+    io.emit('chat', data)
+  })
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
 // Cache Headers
 app.use((req, res, next) => {
   res.set('Cache-control', 'public, max-age=300')
@@ -23,4 +37,4 @@ app.use((req, res, next) => {
 app.use(compression())
 
 // Listen to port
-app.listen(PORT, () => console.log(`Listening on http://[::]:${PORT}`))
+http.listen(PORT, () => console.log(`Listening on http://[::]:${PORT}`))
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,20 @@
-console.log('Client Works!')
+let socket = io()
+let messages = document.querySelector('#message-container')
+let messageInput = document.querySelector('#message-input')
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault()
+    socket.emit('chat', {
+      input: messageInput.value,
+      time: new Date().toLocaleDateString([], {year: 'numeric', month: 'numeric', day: 'numeric'})
+  })
+  messageInput.value = ''
+})
+
+socket.on('chat', data => {
+  messages.innerHTML += `
+    <h2>Name <span class="time">${data.time}</span></h2>
+    <p>${data.input}</p>
+  `
+  messages.scrollTop = messages.scrollHeight
+})
