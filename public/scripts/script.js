f4a54767c54bc2b1221c604ff2364bfec64ccc34
DphnZwp <zwuupdaphne@gmail.com>
2022-06-03 14:13:50 +0200

    created new branche

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,11 @@
 // Variables
+
 let socket = io()
 let messages = document.querySelector('#message-container')
 let messageInput = document.querySelector('#message-input')
 let messageForm = document.querySelector('form')
-let name = prompt('What is your name?')
-let date = new Date().toLocaleDateString([], {year: 'numeric', month: 'numeric', day: 'numeric'})
+let name = localStorage.getItem('name') || prompt('What is your name?')
+let date = new Date().toLocaleDateString([], { year: 'numeric', month: 'numeric', day: 'numeric' })
 
 // Eventlisteners and Function Decleration
 
@@ -12,49 +13,65 @@ messageForm.addEventListener('submit', (e) => {
   e.preventDefault()
   let message = messageInput.value
   messages.insertAdjacentHTML(
-		'beforeend',
-		`
+    'beforeend',
+    `
 		<li>
-      <img src="/assets/images/avatar2.svg" alt="Avatar">
+      <span class="circle">${name.charAt(0)}</span>
       <div class="message">
         <h2>${name} ${date}</h2>
         <p>${message}</p>
       </div>
 		</li>
 	`
-	)
+  )
   socket.emit('send-chat-message', message)
   messageInput.value = ''
+  messages.scrollTo(0, messages.scrollHeight)
 })
 
-appendMessage(`${name} has connected`)
+localStorage.setItem('name', name)
 
-// SOCKET.IO Functions
+renderWelcomeMessage()
+
+// Socket.io Functions
 
 socket.emit('new-user', name)
 
-socket.on('chat-message', (data) => {
-  appendMessage(`${data.name} ${data.date} : ${data.message}`)
+socket.on('user-connected', (name) => {
+  messages.insertAdjacentHTML(
+    `beforeend`,
+    `<li>${name} has joined the chat!</li>`
+  )
 })
 
-socket.on('user-connected', (name) => {
-  appendMessage(`${name} has connected`)
+socket.on('chat-message', (data) => {
+  messages.insertAdjacentHTML(
+    'beforeend',
+    `
+		<li>
+      <span class="circle circle-received">${data.name.charAt(0)}</span>
+      <div class="message">
+        <h2>${data.name} ${date}</h2>
+        <p>${data.message}</p>
+      </div>
+		</li>
+	`
+  )
+  messages.scrollTo(0, messages.scrollHeight)
 })
 
 socket.on('user-disconnected', (data) => {
-  appendMessage(`${name} has disconnected`)
+  messages.insertAdjacentHTML(
+    `beforeend`,
+    `<li>${name} has left the chat!</li>`
+  )
 })
 
 // Functions
 
-function appendMessage(message) {
-  let messageEl = document.createElement('li')
-  messageEl.innerText = message
-  messages.append(messageEl)
-}
-
-function receivedMessage(message) {
-  let receivedMessageEl = document.createElement('li')
-  receivedMessageEl.innerText = message
-  messages.append(receivedMessageEl)
+function renderWelcomeMessage() {
+  messages.insertAdjacentHTML(
+    `beforeend`,
+    `<li class="welcome-message">Welcome ${name}, have a great time talking to everybody!</li>`
+  )
 }
