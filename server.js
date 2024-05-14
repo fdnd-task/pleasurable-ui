eaf4f1801484013c6c4d970b802ec9e3f29da8fd
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-05-14 11:58:50 +0200

    CW1.1 main-server routes

--- server.js
+++ server.js
@@ -1,1 +1,110 @@
-console.log('Hier komt jullie server')
+console.log('Hier komt jullie server')import express from "express";
+
+const app = express();
+
+import fetchJson from "./helpers/fetch-json.js";
+import { promises } from "dns";
+
+// main HTML files and the public files
+app.set("views", "./views");
+app.use(express.static("./public/"));
+
+// ejs
+app.set("view engine", "ejs");
+
+// dont know what this does
+app.use(express.urlencoded({ extended: true }));
+// Redpers database //
+
+// main Url
+const apiUrl = "https://redpers.nl/wp-json/wp/v2";
+
+//directus url
+const directus_apiUrl = "https://fdnd-agency.directus.app/items/redpers_shares";
+// const directus_filterUrl = `${directus_apiUrl}?filter[id][_eq]=${request.params.id}`;
+
+//End points //
+
+//post
+const postsUrl = `${apiUrl}/posts?per_page=100&orderby=date&order=desc`;
+//users
+const usersUrl = `${apiUrl}/users?_embed`;
+//categories
+const categoryUrl = `${apiUrl}/posts?per_page=100&orderby=date&order=desc&categories=1,4,6,9,63,94,1010,3211,7164&_fields=id,categories,slug,date_gmt,excerpt,status,author,yoast_head_json`;
+const categoriesUrl = `${apiUrl}categories?per_page=100`
+// GET route for index
+app.get("/",function(req,res){
+	Promise.all([
+		fetchJson(postsUrl),
+		fetchJson(usersUrl),
+		fetchJson(categoryUrl)
+	])
+	.then(([postData,userData,categoryData]) => {
+
+		// all the other functions
+		// datePars(); // fix this parser issue
+
+		res.render("index.ejs", {
+			postData,
+			userData,
+			categoryData});
+		
+			console.log("home success");
+
+	})
+	// .catch(err => console.log(err,res.status(500).send("Error fetching data")));
+});
+
+// GET route for post
+app.get("/post/:id",function(req,res){
+
+	res.render("post.ejs",{})
+	res.redirect(303, '/');
+
+});
+
+
+
+
+
+
+// GET route for category list
+app.get("/categories",function(req,res){
+
+	res.render("category.ejs",{})
+	res.redirect(303, '/');
+
+});
+
+// GET route for individual category 
+app.get("/category/:slug",function(req,res){
+
+	res.render("category.ejs",{})
+	res.redirect(303, '/');
+
+});
+
+// GET route for author list
+app.get("/authors",function(req,res){
+
+	res.render("authors.ejs",{})
+	res.redirect(303, '/');
+
+});
+
+// GET route for individual author 
+app.get("/author/:id",function(req,res){
+
+	res.render("author.ejs",{})
+	res.redirect(303, '/');
+
+})
+
+// ports //
+app.set("port", process.env.PORT || 808);
+
+app.listen(app.get("port"), function(){
+	console.log(`Test link ${app.get("port")}`);
+	console.log("Server listening on port " + '👉 ' + `http://localhost:${app.get("port")}`+ ' 👈');
+});
+
