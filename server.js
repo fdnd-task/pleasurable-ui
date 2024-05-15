f8765e4d797ed9ce7f2f9454d7914dbe3d087691
RukiyaTossou <114156045+RukiyaTossou@users.noreply.github.com>
2024-05-15 11:21:47 +0200

    v1.1 Basis code van de server.js

--- server.js
+++ server.js
@@ -1,1 +1,39 @@
-console.log('Hier komt jullie server')
+// Importeer het npm pakket express uit de node_modules map
+import express from 'express'
+
+// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
+import fetchJson from './helpers/fetch-json.js'
+
+// Maak een nieuwe express app aan
+const app = express()
+
+
+//endpoints
+const storiesAPI = await fetchJson('https://fdnd-agency.directus.app/items/tm_story');
+const playlistsAPI = await fetchJson('https://fdnd-agency.directus.app/items/tm_playlist');
+const audioAPI = await fetchJson('https://fdnd-agency.directus.app/items/tm_audio');
+
+
+
+
+// Stel ejs in als template engine
+app.set('view engine', 'ejs')
+
+// Stel de map met ejs templates inx
+app.set('views', './views')
+
+// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
+app.use(express.static('public'))
+
+// Zorgt dat werken met request data makkelijker wordt
+app.use(express.urlencoded({extended: true}))
+
+
+// Stel het poortnummer in waar express op moet gaan luisteren
+app.set('port', process.env.PORT || 8000)
+
+// Start express op, haal daarbij het zojuist ingestelde poortnummer op
+app.listen(app.get('port'), function() {
+  // Toon een bericht in de console en geef het poortnummer door
+  console.log(`Application started on http://localhost:${app.get('port')}`)
+})
