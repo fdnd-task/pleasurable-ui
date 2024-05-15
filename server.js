16e6f6fc81c26902281e72980b82f7159ef90fec
christoph3r3w <144007933+christoph3r3w@users.noreply.github.com>
2024-05-15 11:14:37 +0200

    CW1.3 server changes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log('Hier komt jullie server')import express from "express";
+import express from "express";
 
 const app = express();
 
@@ -48,6 +48,21 @@ const usersUrl = `${apiUrl}/users?_embed`;
 //categories
 const categoryUrl = `${apiUrl}/posts?per_page=100&orderby=date&order=desc&categories=1,4,6,9,63,94,1010,3211,7164&_fields=id,categories,slug,date_gmt,excerpt,status,author,yoast_head_json`;
 const categoriesUrl = `${apiUrl}categories?per_page=100`
+
+// functions
+// const datePars = function(){ // ask how to implement this function and fix the post data
+// 	for(var i = 0; i < postData.length;i++){
+// 	const parsedDate = new Date(postData[i].date),
+// 	day = parsedDate.getDate(),
+// 	options = { month: "short" }, // De maand moet kort geschreven zijn
+// 	month = Intl.DateTimeFormat("nl-NL", options).format(parsedDate.getMonth() + 1),
+// 	newDate = day + ''+ month;
+// 	postData[i].date = newDate;
+// 	categoryData[i].date = newDate;
+// 	postData[i].author = userData.find(user => user.id === postData[i].author);
+// 	console.log('date parser works');
+// }}
+
 // GET route for index
 app.get("/",function(req,res){
 	Promise.all([
@@ -79,6 +94,69 @@ app.get("/post/:id",function(req,res){
 
 });
 
+// POST route for post
+// likes
+app.post("/post/:id/likes",function(req,res){
+	fetchJson(`${directus_apiUrl}/?filter[id][_eq]=${req.params.id}`).then(({data})=>{
+		
+		// let newLikes = data.length == 0? 1 : data[0].likes >= 1? 0 : data[0].likes + 1;
+
+		const x = data[0];
+        const newLikes = (x.likes >= 1) ? 0 : 1;
+
+		if( req.body.action == 'like' && newLikes === 1){
+			fetchJson(`${directus_apiUrl}/${data[0].id? data[0].id : ''}`,{
+				method: POST,
+				// if the post exists, update it, otherwise create it
+				// method: data[0]?.id ? "PATCH" : "POST",
+				headers: {"content-type":"application/json"} ,
+				body: JSON.stringify({
+					id: request.params.id,
+					likes: newLikes,
+				}) ,
+			}).then((result)=>{
+				console.log(result);
+
+				if(req.body.enhanced){
+					res.render('./partials/likes.ejs',{
+						post :{id: req.params.id},
+						likes: { newLikes},
+
+					})
+				} else{
+					res.redirect(303, `/post/${req.params.id}`);
+				}
+
+
+			})
+		} else if(req.body.action === 'like' && newLikes === 0) {
+			fetchJson(`${directus_apiUrl}/${data[0].id? data[0].id : ''}`,{
+				method: DELETE,
+				Headers: {"content-type":"application/json"} ,
+				
+			}).then((result)=>{
+				console.log(result);
+
+				if(req.body.enhanced){
+					res.render('./partials/likes.ejs',{
+						post :{id: req.params.id},
+						likes: {newLikes},
+
+					})
+				} else{
+					res.redirect(303, `/post/${req.params.id}`);
+				}
+
+
+			})
+
+		}
+		
+		
+		
+		// res.render("post.ejs",{})
+	})
+})
 
 
 
@@ -94,6 +172,7 @@ app.get("/categories",function(req,res){
 
 // GET route for individual category 
 app.get("/category/:slug",function(req,res){
+
 
 	res.render("category.ejs",{})
 	res.redirect(303, '/');
