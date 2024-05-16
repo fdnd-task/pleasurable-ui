1346b9128e81cebb22dbf26d78c7f3b982cee633
TristanKatte <134813224+TristanKatte@users.noreply.github.com>
2024-05-16 10:13:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 console.log('Hier komt jullie server')
+import express from 'express'
+import fetchJson from './helpers/fetch-json.js'
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+// Stel het basis endpoint in
+const apiUrl = 'https://fdnd-agency.directus.app/items';
+
+
+
+
+
+
+// Maak een GET route voor de index
+app.get('/', function (request, response) {
+    fetchJson(sdgList).then((sdgDataUitDeAPI) => {
+        fetchJson(companies).then((companiesUitDeAPI) => {
+            response.render("index", {sdgs: sdgDataUitDeAPI.data, companies: companiesUitDeAPI.data});
+        });  
+    });
+});
