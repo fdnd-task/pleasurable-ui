7afbdbb6a34974e23925c63bf59b9afd4d28d937
Daan645 <daan@videotechniek.nl>
2024-05-16 14:25:25 +0200

    content wrapper & detailsActive function
    Ik heb een content wrapper gemaakt waardoor ik de layout kan veranderen wanneer de details op het scherm komen. Ook heb ik hiervoor class.toggles aangemaakt in javascript waardoor de functie nu zonder animatie werkt.

--- public/daan-animations.js
+++ public/daan-animations.js
@@ -13,12 +13,34 @@ carouselTimeline
     .from('.favourites', {duration: 0.5, x: '100%', ease: 'ease-in'})
     .from('.borrowed', {duration: 0.5, x: '-100%', ease: 'ease-in'})
 
+
+
+
+
+
 // selecteer de button
 const forYouDetailsButton = document.getElementById('forYoudetails')
+const forYouDetails = document.querySelector('.for-you-details')
+const contentWrapperActive = document.querySelector('.content-wrapper')
+
 // zet er een click event op en selecteer het object timeline
 forYouDetailsButton.addEventListener('click', () => {
     // zorgt ervoor dat de animaties in de timeline 3x zo snel gaan
     carouselTimeline.timeScale(2)
     // reverse de animatie op de timeline met .reverse
     carouselTimeline.reverse()
+
+    detailsForYouActive()
 })
+
+function detailsForYouActive(){
+    forYouDetails.classList.add('for-you-details-active')
+    forYouDetails.classList.remove('for-you-details')
+    contentWrapperActive.classList.add('content-wrapper-active')
+    console.log('ik werk')
+}
+
+function activeContentWrapper(){
+    contentWrapperActive.classList.add('content-wrapper-active')
+    console.log('wrapper')
+}
