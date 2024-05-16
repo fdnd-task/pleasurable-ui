84b4fdd549ec8031101783f31583da72fff55180
Daan645 <daan@videotechniek.nl>
2024-05-16 16:13:32 +0200

    Werkende detail functie
    Ik heb ervoor gezorgd dat de detail functie nu werkt, en verschijnt wanneer de animatie afgerond is.

--- public/daan-animations.js
+++ public/daan-animations.js
@@ -1,46 +1,49 @@
 // personal page load animations
-gsap.from('main', {duration: 0.7, y: '100%', ease: 'ease-in'})
-gsap.from('h1', {duration: 0.6, y: '100%', delay: 0.7, ease: 'ease-out'})
-gsap.from('.hamburger-menu', {duration: 0.5, y: '-150%', delay: 0.7, ease: 'bounce'})
-gsap.from('.oba-logo', {duration: 0.8, y: '-130%', delay: 0.7, ease: 'bounce'})
+gsap.from('main', {duration: 0.7, y: '100%', ease: 'ease-in'});
+gsap.from('h1', {duration: 0.6, y: '100%', delay: 0.7, ease: 'ease-out'});
+gsap.from('.hamburger-menu', {duration: 0.5, y: '-150%', delay: 0.7, ease: 'bounce'});
+gsap.from('.oba-logo', {duration: 0.8, y: '-130%', delay: 0.7, ease: 'bounce'});
 
 
 // timeline for the carousel animation
-const carouselTimeline = gsap.timeline({defaults: { duration: 1 }})
+const carouselTimeline = gsap.timeline({defaults: { duration: 1 }});
 
 carouselTimeline
     .from('.for-you', {duration: 0.5, x: '-100%', delay: 0.7, ease: 'ease-in'})
     .from('.favourites', {duration: 0.5, x: '100%', ease: 'ease-in'})
-    .from('.borrowed', {duration: 0.5, x: '-100%', ease: 'ease-in'})
+    .from('.borrowed', {duration: 0.5, x: '-100%', ease: 'ease-in'});
 
+// timeline for for you
+const forYouTimeline = gsap.timeline({defaults: { duration: 1 }});
 
-
-
+// forYouTimeline
+// .from('.for-you-details', {duration: 0.7, y: '100%', ease: 'ease-in', delay: 0.1});
 
 
 // selecteer de button
-const forYouDetailsButton = document.getElementById('forYoudetails')
-const forYouDetails = document.querySelector('.for-you-details')
-const contentWrapperActive = document.querySelector('.content-wrapper')
+const forYouDetailsButton = document.getElementById('forYoudetails');
+const forYouDetails = document.querySelector('.for-you-details');
+const contentWrapperActive = document.querySelector('.content-wrapper');
 
-// zet er een click event op en selecteer het object timeline
 forYouDetailsButton.addEventListener('click', () => {
-    // zorgt ervoor dat de animaties in de timeline 3x zo snel gaan
-    carouselTimeline.timeScale(2)
-    // reverse de animatie op de timeline met .reverse
-    carouselTimeline.reverse()
+    // Zorg ervoor dat de animaties in de timeline 3x zo snel gaan
+    carouselTimeline.timeScale(2);
 
-    detailsForYouActive()
-})
+    // Reverse de animatie op de timeline met .reverse
+    carouselTimeline.reverse().eventCallback("onReverseComplete", () => {
+        detailsForYouActive();
+        activeContentWrapper();
+    });
+});
 
 function detailsForYouActive(){
-    forYouDetails.classList.add('for-you-details-active')
-    forYouDetails.classList.remove('for-you-details')
-    contentWrapperActive.classList.add('content-wrapper-active')
-    console.log('ik werk')
+    forYouDetails.classList.add('for-you-details-active');
+    forYouDetails.classList.remove('for-you-details');
+    contentWrapperActive.classList.add('content-wrapper-active');
+    console.log('ik werk');
 }
 
 function activeContentWrapper(){
-    contentWrapperActive.classList.add('content-wrapper-active')
-    console.log('wrapper')
+    contentWrapperActive.classList.add('content-wrapper-active');
+    console.log('wrapper');
 }
