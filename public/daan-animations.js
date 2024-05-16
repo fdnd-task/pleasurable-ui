a1fa9d82862745fac486a778f2d3f35092411e5d
Daan645 <daan@videotechniek.nl>
2024-05-16 13:33:39 +0200

    Carousel timeline & Reverse function
    Ik heb de animatie van de carousels in een gsap timeline gezet. Vervolgens heb ik er voor gezorgd dat de carousels een reverse functie hebben waardoor er ruimte is voor de details.

--- public/daan-animations.js
+++ public/daan-animations.js
@@ -3,8 +3,22 @@ gsap.from('main', {duration: 0.7, y: '100%', ease: 'ease-in'})
 gsap.from('h1', {duration: 0.6, y: '100%', delay: 0.7, ease: 'ease-out'})
 gsap.from('.hamburger-menu', {duration: 0.5, y: '-150%', delay: 0.7, ease: 'bounce'})
 gsap.from('.oba-logo', {duration: 0.8, y: '-130%', delay: 0.7, ease: 'bounce'})
-gsap.from('.for-you', {duration: 0.5, x: '-100%', delay: 0.7, ease: 'ease-in'})
-gsap.from('.favourites', {duration: 0.5, x: '100%', delay: 0.7, ease: 'ease-in'})
-gsap.from('.borrowed', {duration: 0.5, x: '-100%', delay: 0.7, ease: 'ease-in'})
 
 
+// timeline for the carousel animation
+const carouselTimeline = gsap.timeline({defaults: { duration: 1 }})
+
+carouselTimeline
+    .from('.for-you', {duration: 0.5, x: '-100%', delay: 0.7, ease: 'ease-in'})
+    .from('.favourites', {duration: 0.5, x: '100%', ease: 'ease-in'})
+    .from('.borrowed', {duration: 0.5, x: '-100%', ease: 'ease-in'})
+
+// selecteer de button
+const forYouDetailsButton = document.getElementById('forYoudetails')
+// zet er een click event op en selecteer het object timeline
+forYouDetailsButton.addEventListener('click', () => {
+    // zorgt ervoor dat de animaties in de timeline 3x zo snel gaan
+    carouselTimeline.timeScale(2)
+    // reverse de animatie op de timeline met .reverse
+    carouselTimeline.reverse()
+})
