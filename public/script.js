a11df8adf159f3d990e9e00c6877afb4a1b7a836
TristanKatte <tristankatte@hotmail.com>
2024-05-17 10:03:46 +0200

    update

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const menu = document.querySelector('nav')
-			const button = document.querySelector('.fdnd-button')
-			const close = document.querySelector('path')
-			const filterdiv = document.querySelector('main div:first-child')
+const button = document.querySelector('.fdnd-button')
+const close = document.querySelector('path')
+const filterdiv = document.querySelector('main div:first-child')
 
 			button.addEventListener('click', function(){
   			menu.classList.add('show');
