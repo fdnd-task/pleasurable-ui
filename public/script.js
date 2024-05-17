6d0b35136d284a2d48d7cc0c88ba6181f6c0cb14
TristanKatte <tristankatte@hotmail.com>
2024-05-17 14:58:57 +0200

    menu responsive gemaakt met Javascript

--- public/script.js
+++ public/script.js
@@ -1,16 +1,16 @@
 // Deze code is puur voor het zij-menu die over de gehele website komt
 
-// const menu = document.querySelector('nav')
-// const button = document.querySelector('.fdnd-button')
-// const close = document.querySelector('path')
-// const filterdiv = document.querySelector('main div:first-child')
+const menu = document.querySelector('.crossnav')
+const button = document.querySelector('.menu-button')
+const close = document.querySelector('path')
+const filterdiv = document.querySelector('main div:first-child')
 
-// 			button.addEventListener('click', function(){
-//   			menu.classList.add('show');
-// 			filterdiv.classList.toggle('filter');
-// });
+			button.addEventListener('click', function(){
+  			menu.classList.add('show');
+			filterdiv.classList.toggle('filter');
+});
 
-// 			close.addEventListener('click', function(){
-//   			menu.classList.remove('show')
-// 			filterdiv.classList.remove('filter');
-// });
+			close.addEventListener('click', function(){
+  			menu.classList.remove('show')
+			filterdiv.classList.remove('filter');
+});
