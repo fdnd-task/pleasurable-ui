a9cb1b630494408e9cea21f33ae1101cf662b8e9
Robin <robin.django@gmail.com>
2024-05-19 13:06:01 +0200

    Added comments to clientside JS

--- public/scripts/favorieten.js
+++ public/scripts/favorieten.js
@@ -1,59 +1,68 @@
-// Prijzen omzetten naar juiste formaat
+// Functie om nummers te converteren voor leesbaarheid (30000 -> 30.000)
 const numberWithPeriods = (value) => {
   return value.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 };
 
+// Zoek alle prijzen op de pagina en voer de eerdere functie hierop uit
 prices = document.querySelectorAll(".price");
 
 prices.forEach((price) => {
+  // Haal eerst het euroteken weg, zodat we enkel de waarde overhouden
+  // Deze zetten we hierna weer terug met een template literal
   const priceValue = price.innerHTML.toString().replace("€", "");
   price.innerHTML = `€${numberWithPeriods(priceValue)}`;
 });
 
 // Clientside POST ratings
 document.querySelectorAll(".rating-form").forEach((form) => {
-  const successElement = form.querySelector(".rating-success .tick");
+  // Dit is een Progressive Enhancement feature om de form te submitten met een eventListener op de radio buttons, maar dit kreeg ik niet werkend
+  // FIXME: Zorg dat de form gesubmit wordt als een radio button geselecteerd wordt, en verberg de submit knop (onderaan de code, buitend de form forEach loop)
 
   // const radios = form.querySelectorAll("input[type=radio]");
-
-  //   radios.forEach((radio) => {
-  //     radio.addEventListener("change", (event) => {
-  //       if (event.target.checked) {
-  //         form.submit();
-  //       }
-  //     });
+  // radios.forEach((radio) => {
+  //   radio.addEventListener("change", (event) => {
+  //     if (event.target.checked) {
+  //       form.submit();
+  //     }
   //   });
+  // });
 
+  // Succes state element
+  const successElement = form.querySelector(".rating-success .tick");
+
+  // Zorg ervoor dat de success state weer verdwijnt na de animatie
   successElement.addEventListener("animationend", () => {
     successElement.classList.remove("draw");
   });
 
+  // On submit event
   form.addEventListener("submit", (event) => {
-    const id = form.dataset.id;
-    const rating = form.querySelector("input[name=rating]:checked").value;
-
+    // Loading state
     const loader = form.querySelector(".loader");
 
     if (loader.classList.contains("hidden")) {
       loader.classList.remove("hidden");
     }
 
-    // Log the request on the clientside console
-    console.log(`Sent POST request 
-    houseID: ${id} 
-    rating: ${rating}`);
+    // Request logic
+    const id = form.dataset.id;
+    const rating = form.querySelector("input[name=rating]:checked").value;
 
     try {
+      // POST request naar de server met de rating 
+      // (URL: "/rate/:id/:rating", e.g. "/rate/6/5")
       fetch(`/rate/${id}/${rating}`, {
         method: "POST",
       })
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
+          // Verberg de loader en toon de success state
           loader.classList.add("hidden");
           successElement.classList.add("draw");
         });
     } catch (error) {
+      // Log eventuele errors en verberg de loader
+      // TODO: Error state
       console.error(error);
       loader.classList.add("hidden");
     }
@@ -62,6 +71,8 @@ document.querySelectorAll(".rating-form").forEach((form) => {
   });
 });
 
-document.querySelectorAll(".rating-form button").forEach((button) => {
-  // button.classList.add("hidden");
-});
+// Onderdeel van de Progressive Enhancement feature voor het submitten op de eventListener van de radio buttons, verbergt de submit knop
+
+// document.querySelectorAll(".rating-form button").forEach((button) => {
+//   button.classList.add("hidden");
+// });
