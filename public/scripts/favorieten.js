8c14c32ec18e1630cd01a44c0a3e0264a3311d0a
Robin <robin.django@gmail.com>
2024-05-21 11:04:06 +0200

    Indentation

--- public/scripts/favorieten.js
+++ public/scripts/favorieten.js
@@ -1,67 +1,71 @@
 // Prijzen omzetten naar juiste formaat
 const numberWithPeriods = (value) => {
-  return value.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    return value.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 };
 
 prices = document.querySelectorAll(".price");
 
 prices.forEach((price) => {
-  const priceValue = price.innerHTML.toString().replace("€", "");
-  price.innerHTML = `€${numberWithPeriods(priceValue)}`;
+    const priceValue = price.innerHTML.toString().replace("€", "");
+    price.innerHTML = `€${numberWithPeriods(priceValue)}`;
 });
+
+// Mobile menu
+
+
 
 // Clientside POST ratings
 document.querySelectorAll(".rating-form").forEach((form) => {
-  const successElement = form.querySelector(".rating-success .tick");
+    const successElement = form.querySelector(".rating-success .tick");
 
-  // const radios = form.querySelectorAll("input[type=radio]");
+    // const radios = form.querySelectorAll("input[type=radio]");
 
-  //   radios.forEach((radio) => {
-  //     radio.addEventListener("change", (event) => {
-  //       if (event.target.checked) {
-  //         form.submit();
-  //       }
-  //     });
-  //   });
+    //   radios.forEach((radio) => {
+    //     radio.addEventListener("change", (event) => {
+    //       if (event.target.checked) {
+    //         form.submit();
+    //       }
+    //     });
+    //   });
 
-  successElement.addEventListener("animationend", () => {
-    successElement.classList.remove("draw");
-  });
+    successElement.addEventListener("animationend", () => {
+        successElement.classList.remove("draw");
+    });
 
-  form.addEventListener("submit", (event) => {
-    const id = form.dataset.id;
-    const rating = form.querySelector("input[name=rating]:checked").value;
+    form.addEventListener("submit", (event) => {
+        const id = form.dataset.id;
+        const rating = form.querySelector("input[name=rating]:checked").value;
 
-    const loader = form.querySelector(".loader");
+        const loader = form.querySelector(".loader");
 
-    if (loader.classList.contains("hidden")) {
-      loader.classList.remove("hidden");
-    }
+        if (loader.classList.contains("hidden")) {
+            loader.classList.remove("hidden");
+        }
 
-    // Log the request on the clientside console
-    console.log(`Sent POST request 
+        // Log the request on the clientside console
+        console.log(`Sent POST request 
     houseID: ${id} 
     rating: ${rating}`);
 
-    try {
-      fetch(`/rate/${id}/${rating}`, {
-        method: "POST",
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data);
-          loader.classList.add("hidden");
-          successElement.classList.add("draw");
-        });
-    } catch (error) {
-      console.error(error);
-      loader.classList.add("hidden");
-    }
+        try {
+            fetch(`/rate/${id}/${rating}`, {
+                method: "POST",
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    console.log(data);
+                    loader.classList.add("hidden");
+                    successElement.classList.add("draw");
+                });
+        } catch (error) {
+            console.error(error);
+            loader.classList.add("hidden");
+        }
 
-    event.preventDefault();
-  });
+        event.preventDefault();
+    });
 });
 
 document.querySelectorAll(".rating-form button").forEach((button) => {
-  // button.classList.add("hidden");
+    // button.classList.add("hidden");
 });
