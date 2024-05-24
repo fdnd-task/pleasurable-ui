5b7eb6dc5a7c177f129e639fc5d0e7a5b2083c45
Naddybs <144009698+Naddybs@users.noreply.github.com>
2024-05-24 09:46:31 +0200

    Posten sterren beoordeling

--- server.js
+++ server.js
@@ -30,12 +30,52 @@ fetchJson(apiUrl).then((apiData) => {
     app.use(express.static('public'));
     app.use(express.urlencoded({ extended: true }));
 
+    // Dit zijn arrays die worden gebruikt om de beoordelingen van de gebruiker op te slaan.
+    // arrays zijn een soort lijsten waarin je meerdere waarden kunt opslaan. In dit geval worden de beoordelingen van de gebruiker opgeslagen in de arrays.
+    const algemeen = [];
+    const locatie = [];
+    const prijs= [];
+    const oppervlakte = [];
+    const ligging = [];
+    const kamers = [];
+
     // Dit zorgt ervoor dat de server de index.ejs file rendert wanneer er een get request wordt gedaan op de root van de server.
     // Vervolgens wordt de data van de API meegegeven aan de view zodat deze kan worden gebruikt in de ejs file.
     // houses: apiData.data.houses is de data die wordt meegegeven aan de view. Deze data is afkomstig van de API.
+    // algemeen, locatie, prijs, oppervlakte, ligging en kamers zijn de arrays die worden meegegeven aan de index.ejs file.
+    // Deze arrays bevatten de beoordelingen van de gebruiker. Die worden met de get request opgehaald en vervolgens meegegeven aan de index.ejs file.
     app.get('/', async (req, res) => {
-        res.render('index', { houses: apiData.data.houses });
+        res.render('index', 
+        { houses: apiData.data.houses,
+          algemeen: algemeen,
+          locatie: locatie,
+          prijs: prijs,
+          oppervlakte: oppervlakte,
+          ligging: ligging,
+          kamers: kamers});
     });
+
+    // Dit zorgt ervoor dat de server de submit-rating route afhandelt wanneer er een post request wordt gedaan op de /submit-rating route.
+    // De push methode wordt gebruikt om een nieuwe waarde toe te voegen aan een array. 
+    // Tot slot wordt er een redirect gedaan naar de homepagina.
+
+    app.post('/submit-rating', (req, res) => {
+        const ratings = req.body;
+       
+        console.log(ratings);
+    
+        // Dit voegt de gegevens toe aan de juiste arrays, dus de beoordelingen van de gebruiker worden hier opgeslagen in de arrays.
+        algemeen.push(ratings.algemeneBeoordeling);
+        locatie.push(ratings.locatie);
+        prijs.push(ratings.prijs);
+        oppervlakte.push(ratings.oppervlakte);
+        ligging.push(ratings.ligging);
+        kamers.push(ratings.kamers);
+    
+        // Stuur een response terug naar de client
+        res.redirect('/');
+    });
+    
 
     // Dit zorgt ervoor dat de server start op poort 3000 en dat er een bericht wordt gelogd in de console wanneer de server is gestart.
     app.set('port', process.env.PORT || 3000);
@@ -43,5 +83,3 @@ fetchJson(apiUrl).then((apiData) => {
         console.log(`Application started on http://localhost:${app.get('port')}`);
     });
 });
-
-// ...
