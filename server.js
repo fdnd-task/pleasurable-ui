1436c347b80b9987bddfe8522cf12b78e70224c2
Kaan <kaan.kalmioglu@hva.nl>
2024-05-24 20:54:15 +0200

    korte refactor
    De routes werken mogelijk niet dynamisch nog, (ben ik eigenlijk vrijwel zeker van). Dit is even voor mij om te kijken hoe en wat ik nodig ga hebben per route!

--- server.js
+++ server.js
@@ -1,37 +1,20 @@
-// Importeer het npm pakket express uit de node_modules map
 import express from 'express'
-
-// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
 import fetchJson from './helpers/fetch-json.js'
 
-// Maak een nieuwe express app aan
 const app = express()
 
-// Stel ejs in als template engine
 app.set('view engine', 'ejs')
-
-// Stel de map met ejs templates in
 app.set('views', './views')
-
-// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static('public'))
-
-// Zorg dat werken met request data makkelijker wordt
 app.use(express.urlencoded({ extended: true }))
-
-// Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8009)
 
-// Start express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
-    // Toon een bericht in de console en geef het poortnummer door
-    console.log(`Application started on http://localhost:${app.get('port')}`)
+ console.log(`Application started on http://localhost:${app.get('port')}`)
 })
 
-// Stel het basis endpoint in
-const apiUrl = 'https://fdnd-agency.directus.app/items'
-
-const sdgData = await fetchJson(apiUrl + '/hf_sdgs?fields=*,icon.id,icon.height,icon.width'),
+const apiUrl = 'https://fdnd-agency.directus.app/items',
+    sdgData = await fetchJson(apiUrl + '/hf_sdgs?fields=*,icon.id,icon.height,icon.width'), 
     stakeholdersData = await fetchJson(apiUrl + '/hf_stakeholders?filter={"company_id":2}'),
     scoresData = await fetchJson(apiUrl + '/hf_scores'),
     companiesData = await fetchJson(apiUrl + '/hf_companies/2')
@@ -41,16 +24,11 @@ console.log(companiesData.data.name)
 // ROUTES -----------------------------------------------------------
 app.get('/', function (request, response) {
     response.render('index', {
-        sdgs: sdgData.data,
-        stakeholder: stakeholdersData.data,
-        score: scoresData.data,
         company: companiesData.data,
     })
 })
 
-// @ruben: verander wat je moet veranderen, denk niet dat ik die routes goed heb gedaan.
-
-app.get('/dashboard/:id', function (request, response) { // @ruben: Moet dit :id zijn of kan :company_id ook ?
+app.get('/dashboard/:company_id', function (request, response) { 
     response.render('dashboard', {
         sdgs: sdgData.data,
         stakeholder: stakeholdersData.data,
@@ -60,8 +38,15 @@ app.get('/dashboard/:id', function (request, response) { // @ruben: Moet dit :id
 })
 
 // VRAGENLIJST -----------------------------------------------------
-app.get('/gegevens-form/:id', function (request, response) { // @ruben: Moet dit :id zijn of kan :stakeholder_type ook ?
-    response.render('dashboard', {
+app.get('/gegevens-form/:stakeholder_type', function (request, response) { 
+    response.render('gegevens-form', {
+        stakeholder: stakeholdersData.data,
+        company: companiesData.data,
+    })
+})
+
+app.get('/sdg-form', function (request, response) { 
+    response.render('sdg-form', {
         sdgs: sdgData.data,
         stakeholder: stakeholdersData.data,
         score: scoresData.data,
@@ -69,8 +54,8 @@ app.get('/gegevens-form/:id', function (request, response) { // @ruben: Moet dit
     })
 })
 
-app.get('/sdg-form', function (request, response) { // @ruben: Geen idee of hier ook :id moet staan ben lost
-    response.render('dashboard', {
+app.get('/score-form', function (request, response) {
+    response.render('score-form', {
         sdgs: sdgData.data,
         stakeholder: stakeholdersData.data,
         score: scoresData.data,
@@ -78,17 +63,8 @@ app.get('/sdg-form', function (request, response) { // @ruben: Geen idee of hier
     })
 })
 
-app.get('/score-form', function (request, response) { // @ruben: etc.
-    response.render('dashboard', {
-        sdgs: sdgData.data,
-        stakeholder: stakeholdersData.data,
-        score: scoresData.data,
-        company: companiesData.data,
-    })
-})
-
-app.get('/done', function (request, response) {// @ruben: etc.
-    response.render('dashboard', {
+app.get('/done', function (request, response) {
+    response.render('done-form', {
         sdgs: sdgData.data,
         stakeholder: stakeholdersData.data,
         score: scoresData.data,
