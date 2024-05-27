1db5329c5400469cfa5e159317e0bf8be058842e
Robin <robin.django@gmail.com>
2024-05-27 14:49:55 +0200

    Added confetti on 5 star rating

--- public/scripts/favorieten.js
+++ public/scripts/favorieten.js
@@ -1,3 +1,11 @@
+let normalizedX = 0;
+let normalizedY = 0;
+
+document.addEventListener("mousemove", function (e) {
+    normalizedX = e.clientX / window.innerWidth;
+    normalizedY = e.clientY / window.innerHeight;
+});
+
 // Functie om nummers te converteren voor leesbaarheid (30000 -> 30.000)
 const numberWithPeriods = (value) => {
     return value.replace(/\B(?=(\d{3})+(?!\d))/g, ".");
@@ -13,6 +21,36 @@ prices.forEach((price) => {
     price.innerHTML = `€${numberWithPeriods(priceValue)}`;
 });
 
+// ---- Confetti effect ----
+
+const confettiDefaults = {
+    spread: 120,
+    // ticks: 20,
+    // gravity: 0,
+    // decay: 0.97,
+    startVelocity: 10,
+    shapes: ["star"],
+    colors: ["FFE400", "FFBD00", "E89400", "FFCA6C", "FDFFB8"],
+};
+
+function shootConfetti() {
+    confetti({
+        ...confettiDefaults,
+        particleCount: 15,
+        scalar: 0.8,
+        shapes: ["star"],
+        origin: { x: normalizedX, y: normalizedY },
+    });
+
+    confetti({
+        ...confettiDefaults,
+        particleCount: 4,
+        scalar: 0.4,
+        shapes: ["circle"],
+        origin: { x: normalizedX, y: normalizedY },
+    });
+}
+
 // ---- Rating form ----
 
 // Zorg ervoor dat de success states weer verdwijnen na de animatie
@@ -21,27 +59,6 @@ document.querySelectorAll(".rating-success .tick").forEach((successEl) => {
         successEl.classList.remove("draw");
     });
 });
-
-/* ---- Footer ---- */
-
-// Voeg een class toe aan de ul elementen om ze zichtbaar te maken als JS werkt
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".footer-col ul").forEach(function (ul) {
-      ul.classList.add("js-enabled");
-    });
-
-    // Voeg click event listeners toe aan de titels
-    document.querySelectorAll(".footer-title").forEach(function (button) {
-      button.addEventListener("click", function () {
-        let ul = this.nextElementSibling;
-        if (ul.style.display === "block") {
-          ul.style.display = "none";
-        } else {
-          ul.style.display = "block";
-        }
-      });
-    });
-  });
 
 // Onderdeel van de Progressive Enhancement feature voor het submitten op de eventListener van de radio buttons, verbergt de submit knop
 // Verberg alle submit knoppen op de rating forms
@@ -63,16 +80,15 @@ document.querySelectorAll(".rating-form").forEach((form) => {
 
             // Als de radio button gecheckt is (want de 'change' event wordt ook uitgevoerd als een radio button unchecked wordt)
             if (event.target.checked) {
+                if (rating == 5) {
+                    // Als de rating 5 is, schiet confetti
+                    shootConfetti();
+                }
                 // Roep de RatingHandler functie aan met de juiste parameters
                 ratingHandler(id, rating, loaderEl, successEl);
             }
         });
     });
-});
-
-// Zorg ervoor dat de success state weer verdwijnt na de animatie
-successElement.addEventListener("animationend", () => {
-    successElement.classList.remove("draw");
 });
 
 // Verberg alle submit knoppen op de rating forms
@@ -89,6 +105,7 @@ function ratingHandler(id, rating, loaderEl, successEl) {
     try {
         // POST request naar de server met de rating
         // (URL: "/rate/:id/:rating", e.g. "/rate/6/5")
+        // throw "Debug error";
         fetch(`/rate/${id}/${rating}`, {
             method: "POST",
         })
@@ -116,22 +133,39 @@ function ratingHandler(id, rating, loaderEl, successEl) {
         }).showToast();
     }
 }
-// document.querySelectorAll(".rating-form button").forEach((button) => {
-//   button.classList.add("hidden");
-// });
-
 
 // ==================================================
 // Dialog venster
 // ==================================================
-const btnLeden = document.querySelector('#btn-leden');
-const dialogLeden = document.querySelector('#dialog-leden');
-const btnCloseDialog = document.querySelector('#btn-close-dialog');
+const btnLeden = document.querySelector("#btn-leden");
+const dialogLeden = document.querySelector("#dialog-leden");
+const btnCloseDialog = document.querySelector("#btn-close-dialog");
 
-btnLeden.addEventListener("click", () =>{
+btnLeden.addEventListener("click", () => {
     dialogLeden.showModal();
 });
 
-btnCloseDialog.addEventListener("click", () =>{
+btnCloseDialog.addEventListener("click", () => {
     dialogLeden.close();
+});
+
+/* ---- Footer ---- */
+
+// Voeg een class toe aan de ul elementen om ze zichtbaar te maken als JS werkt
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".footer-col ul").forEach(function (ul) {
+        ul.classList.add("js-enabled");
+    });
+
+    // Voeg click event listeners toe aan de titels
+    document.querySelectorAll(".footer-title").forEach(function (button) {
+        button.addEventListener("click", function () {
+            let ul = this.nextElementSibling;
+            if (ul.style.display === "block") {
+                ul.style.display = "none";
+            } else {
+                ul.style.display = "block";
+            }
+        });
+    });
 });
