0c22efc709a16bbeba76fa6a36881ee33b68086b
Kitkatisvibing <kate.jadi@gmail.com>
2025-05-12 11:52:43 +0200

    test

--- server.js
+++ server.js
@@ -36,3 +36,5 @@ app.set('port', process.env.PORT || 8000)
 app.listen(app.get('port'), function () {
   console.log(`Project draait via http://localhost:${app.get('port')}/\n\nSucces deze sprint. En maak mooie dingen! 🙂`)
 })
+
+console.log('test van Kate')
