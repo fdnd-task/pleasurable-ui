f3ab773eed4aab2bcb7cdec08e5589a419ea14d6
yamenAl <yamen_alsharabi@hotmail.com>
2025-05-28 01:23:33 +0200

    adding animation to the button #69
    Here, I added animation to the button and also added a popup for navigation.

--- public/assets/scrpits/exercise.js
+++ public/assets/scrpits/exercise.js
@@ -1,38 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const toggle = document.querySelector('.menu-toggle');
-  const menu = document.querySelector('.menu');
-
-  toggle.addEventListener('click', (event) => {
-    event.stopPropagation(); 
-    menu.classList.toggle('open');
-  });
-
-  // Close menu when clicking outside
-  document.addEventListener('click', (event) => {
-    const insideMenu = menu.contains(event.target);
-    const ClickOnToggle = toggle.contains(event.target);
-
-    if (!insideMenu && !ClickOnToggle) {
-      menu.classList.remove('open');
-    }
-  });
-});
-
-
-
-
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
   try {
     const btn = document.getElementById('PlaatsBerichtBtn');
     const overlay = document.getElementById('overlay');
-    const closeBtn = document.getElementById('closeFormButton');
+    const closeBtn = document.getElementById('close-popup');
 
     if (!btn || !overlay || !closeBtn) {
       console.error("Elementen niet gevonden.");
