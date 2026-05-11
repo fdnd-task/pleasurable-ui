9e48f9b9cceb1e62cc5f3ebf7f1532badfb37caf
MaartentePoele <maarten.te.poele@hva.nl>
2026-05-11 11:50:53 +0200

    test commit van Maarten

--- server.js
+++ server.js
@@ -28,6 +28,11 @@ app.get('/', async function (request, response) {
   response.render('index.liquid')
 })
 
+app.get('/', async function (request, response) {
+  response.render('index.liquid')
+  // een extra route van Maarten om te testen
+})
+
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
 app.set('port', process.env.PORT || 8000)
