1459a6e5dba83f50c88d57f13d84161a08e9e122
Roxy <roxyfokker@hotmail.com>
2026-05-12 15:17:43 +0200

    feat: ook andere informatie van het instrument ophalen #48

--- server.js
+++ server.js
@@ -49,6 +49,7 @@ app.post('/instrumenten/nieuw', async function (request, response){
 
 app.get('/actielog', async function (request, response) {
   const params = new URLSearchParams()
+  params.append('fields', '*,instrument.name,instrument.serial_number,instrument.key')
   const logResponse = await fetch(`${logUrl}?${params.toString()}`)
   const logResponseJSON = await logResponse.json()
 
