fa842d1be4c9d100875dea575d4b91efe7453840
Roxy <roxyfokker@hotmail.com>
2026-05-13 07:35:21 +0200

    feat: filter op actie en filter meegeven aan view #48

--- server.js
+++ server.js
@@ -51,10 +51,19 @@ app.get('/actielog', async function (request, response) {
   const params = new URLSearchParams()
   params.append('fields', '*,instrument.name,instrument.serial_number,instrument.key')
   params.append('sort', '-date_created')
+
+  const filter = request.query.filter
+  if (filter && filter !== 'alles') {
+    params.append('filter[type_action][_eq]', filter)
+  }
+
   const logResponse = await fetch(`${logUrl}?${params.toString()}`)
   const logResponseJSON = await logResponse.json()
 
-  response.render('actielog.liquid', { logs: logResponseJSON.data })
+  response.render('actielog.liquid', { 
+    logs: logResponseJSON.data, 
+    activeFilter: filter 
+  })
 })
 
 app.get('/instrumenten/:key', async function (request, response) {
