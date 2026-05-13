4f70e793557bb0e203875dbb63ef3a250cadce76
Svenvandijk22 <s.van.dijk13@hotmail.com>
2026-05-13 13:11:54 +0200

    #7  route angemaakt voor wishlist

--- server.js
+++ server.js
@@ -31,6 +31,12 @@ app.get("/cadeau-overzicht", async function (request, response) {
   response.render("cadeau.liquid");
 });
 
+app.get("/wishlist", async function (request, response) {
+  response.render("cadeau.liquid");
+});
+
+
+
 // Stel het poortnummer in waar Express op moet gaan luisteren
 // Lokaal is dit poort 8000; als deze applicatie ergens gehost wordt, waarschijnlijk poort 80
 app.set("port", process.env.PORT || 8000);
@@ -41,3 +47,4 @@ app.listen(app.get("port"), function () {
     `http://localhost:${app.get("port")}`,
   );
 });
+
