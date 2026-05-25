6f33a3fe23ce10341364026f013241e1fc7f5600
Tjeerd <tj.wokke@gmail.com>
2026-05-25 17:09:12 +0200

    focus and hover states #59
    Also working on the animation trigger when closing the menu without using the close button.

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,24 +1,58 @@
 const menu = document.querySelector('#menu')
 const body = document.body
 const mainContent = document.querySelectorAll('body > *:not(#menu)')
+const closeButton = document.querySelector('#menu .popover-button')
+console.log(closeButton)
 
 menu.addEventListener("toggle", function () {
     const isOpen = menu.matches(":popover-open");
 
     if (isOpen) {
-        requestAnimationFrame(() => menu.classList.add("show"));
+        menu.classList.add("show")
 
         body.style.overflow = 'hidden'
         mainContent.forEach(el => {
             el.inert = true
         })
-
-    } else {
-        requestAnimationFrame(() => menu.classList.remove("show"));
-
-        body.style.overflow = ''
-        mainContent.forEach(el => {
-            el.inert = false
-        })
+    }
+    else {
+        menu.classList.remove("show")
+        body.style.overflow = '';
+        mainContent.forEach(el => el.inert = false);
     }
 })
+
+// listen for closes
+closeButton.addEventListener("click", closePopover)
+// V doesn't work
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        closePopover()
+    }
+})
+
+// close animation
+function closePopover(event) {
+    if (menu.matches(":popover-open")) {
+        // Stops the popover from instantly closing
+        event.preventDefault();
+        menu.classList.remove("show");
+        body.style.overflow = '';
+        mainContent.forEach(el => el.inert = false);
+        // now hide the popover when the going out animation has finished
+        setTimeout(() => menu.hidePopover(), 500);
+    }
+}
+
+
+
+// tried to auto close after transition but it wouldn't work
+// menu.addEventListener("transitionend", function () {
+//     console.log('trans')
+//     const isOpen = menu.matches(":popover-open");
+
+//     if (!isOpen) {
+//         console.log('trans2')
+//         menu.hidePopover();
+//     }
+// });
