e545f35456bcca0de76234801a6b8bdb58d1bb60
Svenvandijk22 <s.van.dijk13@hotmail.com>
2026-05-25 17:13:05 +0200

    #46 reloaden tegenhouden

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -3,3 +3,7 @@ console.log("test");
 document.addEventListener("submit", function(event) {
 
   const form = event.target
+
+  if (!form.classList.contains("like-form")) return
+
+  event.preventDefault()
