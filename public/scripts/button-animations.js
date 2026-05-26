d39e87a6371fb499bd35df7b4e5267b99d01d451
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-05-26 17:02:38 +0200

    #63 removed view-transition for global button use
    To keep progress quick, I have removed view-transition and will make a separate branch for it

--- public/scripts/button-animations.js
+++ public/scripts/button-animations.js
@@ -19,24 +19,3 @@ animationButtons.forEach(button => {
 
     });
 }); 
-
-document.querySelector(".test").addEventListener("click", (event) =>{
-    event.preventDefault();
-    const loadingAnimation = document.querySelector(".page-trans-container");
-    loadingAnimation.classList.add("page-is-loading");
-
-            setTimeout(() => {
-            loadingAnimation.classList.remove("page-is-loading");
-        }, 4500);
-})
-
-// const links = document.querySelectorAll("a");
-
-// links.forEach(button => {
-//     button.addEventListener('click', (event) => {
-//         event.preventDefault();
-
-//         document.querySelector("body").classList.add("is-loading");
-
-//     });
-// });
