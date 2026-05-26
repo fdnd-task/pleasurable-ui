cfa1b8c6a513a9fced1deaa92b89f9afec1d6871
Chewsy <13772434+Chewsy@users.noreply.github.com>
2026-05-26 09:31:39 +0200

    Help request commit @JasinAhmed

--- public/scripts/button-animations.js
+++ public/scripts/button-animations.js
@@ -9,7 +9,6 @@ animationButtons.forEach(button => {
         setTimeout(() => {
             button.classList.remove("is-loading");
             button.classList.add("is-success");
-
             button.textContent = "Verzonden!";
         }, 4000);
 
@@ -20,3 +19,24 @@ animationButtons.forEach(button => {
 
     });
 }); 
+
+document.querySelector(".test").addEventListener("click", (event) =>{
+    event.preventDefault();
+    const loadingAnimation = document.querySelector(".page-trans-container");
+    loadingAnimation.classList.add("page-is-loading");
+
+            setTimeout(() => {
+            loadingAnimation.classList.remove("page-is-loading");
+        }, 4500);
+})
+
+// const links = document.querySelectorAll("a");
+
+// links.forEach(button => {
+//     button.addEventListener('click', (event) => {
+//         event.preventDefault();
+
+//         document.querySelector("body").classList.add("is-loading");
+
+//     });
+// });
