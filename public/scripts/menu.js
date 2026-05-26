8cffc4d4f3880a858a2646e468834d20a451bdb5
Tjeerd <tj.wokke@gmail.com>
2026-05-26 11:57:13 +0200

    Added animations to closing the popover every way #59

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -2,7 +2,11 @@ const menu = document.querySelector('#menu')
 const body = document.body
 const mainContent = document.querySelectorAll('body > *:not(#menu)')
 const closeButton = document.querySelector('#menu .popover-button')
-console.log(closeButton)
+
+
+// Only when JS is active set it to manual
+menu.setAttribute('popover', 'manual')
+menu.classList.add('hide')
 
 menu.addEventListener("toggle", function () {
     const isOpen = menu.matches(":popover-open");
@@ -24,9 +28,23 @@ menu.addEventListener("toggle", function () {
 
 // listen for closes
 closeButton.addEventListener("click", closePopover)
-// V doesn't work
-document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
+// close by pressing esc
+document.addEventListener("keydown", closePopover)
+
+// close by clicking outside of it
+// 
+// Its tricky because you can't listen to mainContent because the backdrop is covering it
+// And backdrop cannot be selected with the queryselector
+document.addEventListener("click", (event) => {
+    // first I ask where the menu is on the website and how big it is
+    // https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+    const rect = menu.getBoundingClientRect();
+    const clickOutside =
+        event.clientX < rect.left ||
+        event.clientX > rect.right ||
+        event.clientY < rect.top ||
+        event.clientY > rect.bottom;
+    if (clickOutside) {
         closePopover()
     }
 })
@@ -34,8 +52,10 @@ document.addEventListener("keydown", (event) => {
 // close animation
 function closePopover(event) {
     if (menu.matches(":popover-open")) {
+        if (event) {
+            event.preventDefault()
+        }
         // Stops the popover from instantly closing
-        event.preventDefault();
         menu.classList.remove("show");
         body.style.overflow = '';
         mainContent.forEach(el => el.inert = false);
@@ -43,16 +63,3 @@ function closePopover(event) {
         setTimeout(() => menu.hidePopover(), 500);
     }
 }
-
-
-
-// tried to auto close after transition but it wouldn't work
-// menu.addEventListener("transitionend", function () {
-//     console.log('trans')
-//     const isOpen = menu.matches(":popover-open");
-
-//     if (!isOpen) {
-//         console.log('trans2')
-//         menu.hidePopover();
-//     }
-// });
