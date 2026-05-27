960e93d2e5151a78c249f6a7ce10c9eab4665bf3
toasterbath0758 <toasterbath0758@gmail.com>
2026-05-27 12:50:14 +0200

    suggesties.js code in de script.js gezet #18

--- public/assets/scripts/script.js
+++ public/assets/scripts/script.js
@@ -57,3 +57,29 @@ if (memojiForm) {
         }
     });
 }
+
+// add loading state
+const deleteForms = document.querySelectorAll(".delete-form")
+const suggestieLijst = document.querySelector(".collection-suggesties")
+
+
+deleteForms.forEach((deleteForm) => {
+  deleteForm.addEventListener("submit", async function (event) {
+    const deleteKnop = deleteForm.querySelector(".delete")
+    const suggestie = document.querySelector(`li:has(form[data-enhanced="${deleteForm.getAttribute('data-enhanced')}"])`)
+    event.preventDefault()
+
+    deleteKnop.classList.add("loading")
+    deleteKnop.textContent = ""
+
+    let formData = new FormData(deleteForm);
+
+    const response = await fetch(deleteForm.action, {
+      method: deleteForm.method,
+      body: new URLSearchParams(formData)
+    })
+
+    suggestie.remove();
+
+  })
+})
